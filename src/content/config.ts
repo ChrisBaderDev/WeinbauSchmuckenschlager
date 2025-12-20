35fe06786d1d3217a6259630e7761113aab76b9a
Christoph Bader <chrisbaderdev@gmail.com>
2025-12-21 00:35:40 +0100

    Build half of first page

---
+++ src/content/config.ts
@@ -0,0 +1,47 @@
+import { defineCollection, z } from "astro:content";
+
+const wines = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.array(
+      z.object({
+        name: z.string(),
+        year: z.number(),
+        image: image(),
+      })
+    ),
+});
+
+const events = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      cover: image(),
+      gallery: z.array(image()).optional(),
+    }),
+});
+
+const family = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      role: z.string(),
+      image: image(),
+      description: z.string(),
+    }),
+});
+
+const history = defineCollection({
+    type: "content",
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        description: z.string(),
+        image: image(),
+      }),
+  });
+
+export const collections = { wines, events, family, history };
---
+++ src/content/events/valedri_beats_3/index.md
@@ -0,0 +1,8 @@
+---
+title: "Valedri Beats"
+date: 2025-10-25
+cover: ./images/cover.jpg
+gallery:
+  - ./images/image1.jpg
+---
+Your markdown content here...
