48f22cd4e646d9e33cf2a2fe35f76dbce63d2c8b
Christoph Bader <chrisbaderdev@gmail.com>
2026-03-01 12:18:59 +0100

    set canoncial domain to non-www

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'never',
-  site: 'https://www.weinbau-schmuckenschlager.at',
+  site: 'https://weinbau-schmuckenschlager.at',
   vite: {
       plugins: [tailwindcss()],
       build: {
